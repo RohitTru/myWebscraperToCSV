8ad721600b42dc67c6c804dfee0f79db93c7e43e
Rohit Truesdale <rohit.trues@gmail.com>
2023-06-29 18:55:26 -0500

    selenium opens a page

--- main.py
+++ main.py
@@ -1,5 +1,45 @@
+
+
+
+def workingWithSelenium():
+    import time
+    from selenium import webdriver
+    url = 'https://www.youtube.com/'
+
+    driver = webdriver.Chrome()
+    driver.get(url)
+    time.sleep(5)
+    driver.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def main():
-    print('hello world')
+    workingWithSelenium()
 
 if __name__ == "__main__":
     main()
