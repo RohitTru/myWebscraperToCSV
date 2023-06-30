042cfd8a49fc01c8e5ae198f576e967ba74783dd
Rohit Truesdale <rohit.trues@gmail.com>
2023-06-29 23:36:54 -0500

    finished field and xpath validator

--- xPathValidator.py
+++ xPathValidator.py
@@ -1,34 +1,54 @@
-import lxml.etree
+
 from selenium import webdriver
+import time
 
 url = 'https://www.youtube.com/watch?v=lTypMlVBFM4'
 driver = webdriver.Chrome()
 driver.get(url)
 
+time.sleep(5)
+
+fieldNames = []
 xPathList = []
-def validateXpath():
+
+
+def getFieldAndXPath():
+    global fieldNames
     global xPathList
-    i=0
     
+    i=0
     while True:
-        exitMessage = ''
+        # Exit message
+        exitMessage = ""
         if i >= 1:
-            exitMessage = 'or type done to move on '
+            exitMessage = "or type 'done' to move on "
         
-        userInputXpath = input("Please input an xpath " + exitMessage)
+        # Gather field name
+        userInputFieldName = input('Please input a name of a field you would like to scrape ' + exitMessage)
+        fieldNames.append(userInputFieldName)
         i+=1
         
-        if userInputXpath == 'done':
+        # Exit loop
+        if userInputFieldName == 'done':
             break
+        
+        # Gather and validate XPath
+        userInputXpath = input("Please input an xpath: ")
         try:
             driver.find_element('xpath', userInputXpath )
+            
+            # Append to list if program checks out
             xPathList.append(userInputXpath)
 
+            # Toss out the error and try again
         except Exception as e:
             print(f'invalid Xpath: ')
     
-    print('List of valid XPaths:')
-    for xpath in xPathList:
-        print(xpath)        
-        
-validateXpath()
+            
+
+getFieldAndXPath()     
+print(fieldNames)
+print(xPathList)
+time.sleep(50)
+
+
