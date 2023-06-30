29d75e4fe7ee62f12414c4d5b0fb5273e263049d
Rohit Truesdale <rohit.trues@gmail.com>
2023-06-29 19:36:51 -0500

    getUrlFromUser():

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+import requests
+
+url = ''
+def getUrlFromUser():
+    global url
+    while True:
+        
+        # Url Validation
+        try: 
+            userInput = input("Please enter the URL of the website you would like to scrape:\n")
+            page = requests.get(userInput)
+            statusCode = page.status_code
+            
+            if page.status_code == 200:
+                print(f'Status code: {statusCode} connection secured.') 
+                url = userInput
+                return url
+
+        except Exception:
+            print("Your url did not work")
+
+        
+        
+
 
 
 
@@ -39,7 +63,8 @@ def workingWithSelenium():
 
 
 def main():
-    workingWithSelenium()
+    getUrlFromUser()
+    print(url)
 
 if __name__ == "__main__":
     main()
